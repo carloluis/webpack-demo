e3472f47492d567a3510651dc5c0e3f511e433dc
carloluis <carloluisr@gmail.com>
2018-02-21 12:16:25 -0300

    empty webpack configs..

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const TARGET = process.env.npm_lifecycle_event;
+
+if (TARGET === 'dev' || !TARGET) {
+	module.exports = require('./config/webpack.config.dev');
+}
+if (TARGET === 'build' || TARGET === 'stats') {
+	module.exports = require('./config/webpack.config.prod');
+}

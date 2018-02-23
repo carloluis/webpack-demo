e9dc8730b258fb985ebca9ac49c8435777e96c6f
carloluis <carloluisr@gmail.com>
2018-02-22 22:51:29 -0300

    update webpack config (prod)

---
+++ config/webpack.config.prod.js
@@ -0,0 +1,60 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+const package = require('../package.json');
+
+const PATHS = {
+    src: path.join(__dirname, '../src'),
+    dist: path.join(__dirname, '../dist')
+};
+
+module.exports = {
+    context: __dirname,
+    mode: 'production',
+    entry: {
+        app: [PATHS.src],
+        vendors: Object.keys(package.dependencies)
+    },
+    output: {
+        path: PATHS.dist,
+        filename: '[name].[chunkhash].js',
+        publicPath: '/'
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    enforce: true,
+                    chunks: 'all'
+                }
+            }
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: '../node_modules/html-webpack-template/index.ejs',
+            title: 'Webpack 4 Demo',
+            favicon: '../src/favicon.ico',
+            meta: [{ name: 'robots', content: 'noindex,nofollow' }],
+            inject: false,
+            minify: {
+                collapseWhitespace: true,
+                conservativeCollapse: true,
+                preserveLineBreaks: true,
+                useShortDoctype: true,
+                html5: true
+            },
+            mobile: true
+        }),
+        new CopyWebpackPlugin([
+            {
+                from: path.join(PATHS.src, 'favicon.ico'),
+                to: path.join(PATHS.dist, 'favicon.ico')
+            }
+        ])
+    ]
+};

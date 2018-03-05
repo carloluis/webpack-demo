7ad97d17e253149cc62ceef38dad0364d912197b
carloluis <carloluisr@gmail.com>
2018-03-05 16:46:15 -0300

    update webpack scripts.. - running `default:*` (#0CJS)

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const TARGET = process.env.npm_lifecycle_event;
 
-if (TARGET === 'dev' || TARGET === 'dev:server' || !TARGET) {
-	module.exports = require('./config/webpack.config.dev');
+if (TARGET === 'build:dev' || TARGET === 'dev' || !TARGET) {
+    module.exports = require('./config/webpack.config.dev');
+    console.info('--> ./config/webpack.config.dev.js');
 }
 if (TARGET === 'build' || TARGET === 'stats') {
-	module.exports = require('./config/webpack.config.prod');
+    module.exports = require('./config/webpack.config.prod');
+    console.info('--> ./config/webpack.config.prod.js');
 }

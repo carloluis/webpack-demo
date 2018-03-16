4bf30127f76171b4bffd4c287d5bb4da4a051203
carloluis <carloluisr@gmail.com>
2018-03-15 21:12:01 -0300

    add sample <App/> react component

---
+++ src/app/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './App.scss';
+
+const App = () => (
+    <div className={styles.container}>
+        <h1>Webpack 4 Demo!</h1>
+    </div>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import reformat from 'reformat-number';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 
+import App from './app/App';
 import foo from './demo/foo';
 import bar from './demo/bar';
 import './index.scss';
 
+render(<App />, document.querySelector('#app'));
+
 console.info(reformat('1123581321'));
 foo();
-
-ReactDOM.render(<h1>Webpack 4 Demo!</h1>, document.querySelector('#app'));

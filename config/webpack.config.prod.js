b86bb6880c87c647ccbdcc0c5979c6cb41c11d42
carloluis <carloluisr@gmail.com>
2018-03-18 22:41:59 -0300

    add support for react (prod.)

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -55,6 +55,11 @@ module.exports = {
                         }
                     ]
                 })
+            },
+            {
+                test: /.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
             }
         ]
     },

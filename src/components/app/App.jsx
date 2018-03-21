1b92894c47d0372d7c9ef7319e444c358f065c7e
carloluis <carloluisr@gmail.com>
2018-03-21 17:26:12 -0300

    add github icon :octocat:

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Cards from '../cards/Cards';
+import GitHub from '../github/GitHub';
 import styles from './App.scss';
 
 const App = ({ title }) => (
     <div className={styles.container}>
+        <GitHub />
         <h1>{title}</h1>
         <Cards />
     </div>
---
+++ src/components/github/Github.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Github.scss';
+
+const GitHub = () => (
+    <a className={styles.github} href="https://github.com/carloluis/webpack-demo" />
+);
+
+export default GitHub;

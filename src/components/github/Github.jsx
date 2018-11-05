90fe1bcb427f91aad949ef8b4dcaed38ca22bad0
carloluis <carloluisr@gmail.com>
2018-11-06 00:07:33 +0100

    updating react stateless components..

--- src/components/github/Github.jsx
+++ src/components/github/Github.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import styles from './Github.scss';
 
-const GitHub = () => (
-    <a className={styles.github} href="https://github.com/carloluis/webpack-demo" />
-);
+const GitHub = React.memo(() => <a className={styles.github} href="https://github.com/carloluis/webpack-demo" />);
 
 export default GitHub;
